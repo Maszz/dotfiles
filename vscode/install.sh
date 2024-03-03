ff952f1e6ec075b93520a92aaba3b45e3f77c173
Maszz <mawin38408@gmail.com>
2024-03-04 00:36:01 +0700

    feat: add zed and vscode

---
+++ vscode/install.sh
@@ -0,0 +1,4 @@
+set -e
+
+ln -sf "${DOTFILES_LOCATION}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+ln -sf "${DOTFILES_LOCATION}/vscode/keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
